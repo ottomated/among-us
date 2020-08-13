deb264e4d9b391a991b9e724e23838190a01703b
Ottomated <otto@ottomated.net>
2020-08-13 15:49:13 -0700

    fix

--- commands/overlay.js
+++ commands/overlay.js
@@ -20,7 +20,7 @@ class OverlayCommand extends Command {
 	async exec(msg, args) {
 		console.log(msg, args);
 		if (!args.channel) {
-			return msg.channel.send(`Please supply a channel to generate the overlay link for, i.e. \`!overlay ${msg.guild.channels.cache.find(c => c.type === 'voice')?.name}\``);
+			return msg.channel.send(`Please supply a channel to generate the overlay link for, i.e. \`!overlay ${msg.guild.channels.cache.find(c => c.type === 'voice').name}\``);
 		}
 		return msg.channel.send(`**Overlay for ${args.channel.name}:**\n\nhttps://streamkit.discord.com/overlay/voice/${msg.guild.id}/${args.channel.id}?icon=true&online=true&logo=white&text_color=%23ffffff&text_size=28&text_outline_color=%23000000&text_outline_size=0&text_shadow_color=%23000000&text_shadow_size=0&bg_color=%231e2124&bg_opacity=0&bg_shadow_color=%23000000&bg_shadow_size=0&invite_code=jreTYZS&limit_speaking=true&small_avatars=false&hide_names=false&fade_chat=0`)
 	}
